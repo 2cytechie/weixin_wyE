2150fd1f3044935d42652a8a7bff7dfb05e6dafc
2cytechie <3302636082@qq.com>
2025-04-15 17:35:06 +0800

    更新福利页面

--- miniprogram/pages/promotion/promation.js
+++ miniprogram/pages/promotion/promation.js
@@ -1,9 +1,11 @@
 // js
+const db = wx.cloud
 Page({
   data:{
     takeout_data:{
       name:"",
-      phone:""
+      phone:"",
+      make_promation_time:""
     }
   },
   set_message_data(e) {
@@ -14,9 +16,60 @@ Page({
     this.setData(dataToUpdate);
     console.log(this.data.takeout_data)
   },
-
-  submitApply() {
-    // 这里添加提交逻辑，如表单验证、接口调用等
-    console.log('提交申请');
+  order() {
+    // 检查信息是否为空
+    const requiredFields = {
+      name:"昵称",
+      phone:"手机号"
+    }; // 需要检查的字段
+    for (const field in requiredFields) {
+      if (!this.data.takeout_data[field]) {
+          wx.showToast({
+              title: `请输入${requiredFields[field]}`,
+              icon: 'none'
+          });
+          return;
+      }
+  }
+    wx.showModal({
+        title: '提交申请？',
+        content: '',
+        complete: (res) => {
+            if (res.cancel) {
+                // 取消
+            }
+            if (res.confirm) {
+                const now = new Date();
+                const uploadTime = now.toLocaleString();
+                this.setData({
+                  'takeout_data.make_promation_time': uploadTime
+                });
+                // 上传信息
+                db.database().collection("promation_data").add({
+                    data: this.data.takeout_data,
+                    success: (res) => {
+                      wx.showToast({
+                        title: '提交成功',
+                        icon: 'success',
+                        success: () => {
+                          setTimeout(() => {
+                            wx.switchTab({
+                              url: '/pages/my/my',
+                            });
+                          }, 1500);
+                        }
+                      });
+                    },
+                    fail: (err) => {
+                        console.error('申请失败:', err);
+                        wx.showToast({
+                            title: '申请失败,请检查输入信息或网络',
+                            icon: 'none'
+                        });
+                    }
+                });
+            }
+        }
+    });
   }
 })
